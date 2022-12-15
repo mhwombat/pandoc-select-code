dab114cb40142738823789393e184ae5890a63f1
Amy de Buitléir <amy@nualeargais.ie>
2022-12-15 16:57:30 +0000

    reduce nixos global installs

--- default.nix
+++ default.nix
@@ -1,6 +1,12 @@
-let 
+let
   pkgs = import <nixpkgs> { };
-in 
+in
   pkgs.haskellPackages.developPackage {
     root = ./.;
+    modifier = drv:
+      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
+        [
+          cabal-install
+          # list additional tools here
+        ]);
   }
